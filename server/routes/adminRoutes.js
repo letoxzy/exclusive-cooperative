7b9938768cd86e7643e2af099f7a8300b43bc98f
Opokiti Paul <opokitipaul@gmail.com>
2026-08-31 13:52:12 +0100

    Add Cloudinary uploads for existing members

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -25,7 +25,7 @@ const router = express.Router();
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 10 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
